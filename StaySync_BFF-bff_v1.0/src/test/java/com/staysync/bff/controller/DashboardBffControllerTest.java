c354c712cc76c2862a16dd89d16998878ad2e940
angel guillen <an.guillen@duocuc.cl>
2026-06-15 17:15:30 -0400

    feat: agregado prueba unitarias para JwtService y AuthBffController

--- StaySync_BFF-bff_v1.0/src/test/java/com/staysync/bff/controller/DashboardBffControllerTest.java
+++ StaySync_BFF-bff_v1.0/src/test/java/com/staysync/bff/controller/DashboardBffControllerTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,9 +28,9 @@ class DashboardBffControllerTest {
     @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper objectMapper;
 
-    @MockBean ReservasClient reservasClient;
-    @MockBean HabitacionesClient habitacionesClient;
-    @MockBean JwtService jwtService;
+    @MockitoBean ReservasClient reservasClient;
+    @MockitoBean HabitacionesClient habitacionesClient;
+    @MockitoBean JwtService jwtService;
 
     @Test
     @DisplayName("GET /bff/dashboard - debe retornar datos agregados correctamente")
